8d8995ebb4c759ad8d05bc4757fe97243ef27515
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2022-06-11 21:47:54 +0200

    Add files via upload

---
+++ coffee shop/js/script.js
@@ -0,0 +1,22 @@
+var coffee{
+    ime: "nicaragua red coffee";
+    cena: "3$";
+    printInfo: function() {
+    document.write("kafaaa");
+    }
+}
+function myTest(){
+    console.log("uspešna razervacija");
+    
+}
+
+var x = 5;
+
+function myTest(){
+  console.log("x = " + x);
+  console.log("Kafa:" + coffee.ime + " " + coffee.cena);
+  coffee.printInfo();
+}
+
+ 
+
